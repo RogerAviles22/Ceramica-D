7f0d4a1ccecb05f8de9b2a9905eb7c547fd78ca8
RogerAviles22 <rogiavil@espol.edu.ec>
2021-03-06 13:41:07 -0500

    Correcion de dependencias e imagen.

--- src/componentes/body/Welcome.js
+++ src/componentes/body/Welcome.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import collage from "../../assets/img/productos/nuevo/collage.png"
-import {fadeIn} from 'react-animations'
+import collage from "../../assets/img/productos/collage.png"
 
 class Welcome extends Component {
     render() {
         return (
             <section className="row d-flex justify-content-center align-items-center py-3">
-                <img className="welcome col-xl-3 col-md-5 col-12 px-0 shadow" style={fadeIn} src={collage} alt="collage1.jpg"/>
+                <img className="welcome col-xl-3 col-md-5 col-12 px-0 shadow" src={collage} alt="collage1.jpg"/>
                 <p className="col-xl-4 col-md-5 col-12 mt-2 text-center">Pequeño emprendimiento dedicado a la creación de productos cerámicos realizados de forma artesanal.</p>
             </section>
         );
